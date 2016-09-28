f1dee9342078a5392f4e50cf2400b0b3b318c643
mathewsanishalex <mathewsanishalex@gmail.com>
2016-09-28 20:12:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/page1',function(req,res) {
+app.get('/page1.html',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'page1.html')); 
 });
 app.get('/ui/style.css', function (req, res) {
